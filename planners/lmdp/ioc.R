0ad4f6857f32bfeb5b357ecf2c3a41eaa89b42ce
Loren Champlin <champlin@email.arizona.edu>
2024-04-11 17:37:38 -0700

    Cleaned up code

--- planners/lmdp/ioc.R
+++ planners/lmdp/ioc.R
@@ -1,4 +1,5 @@
 source("~/busy-beeway/planners/lmdp/lmdp.R")
+source("~/busy-beeway/planners/lmdp/ioc_data.R")
 
 logsumexp <- function(X) {
   c <- max(X)
@@ -8,96 +9,6 @@ logsumexp <- function(X) {
 rdunif <- function(n,a,b) {
   rg <- a:b
   sample(rg,n,replace = TRUE,prob=rep(1/length(rg),length(rg)))
-}
-
-process_simulated_run <- function(p_pos,g,gb,O,delay=8) {
-  dat <- list()
-  dp <- create_uniform_default_policy(delay)
-  current_p <- c(p_pos[1,1],p_pos[1,2])
-  c_board <- simulate_board(current_p[1],current_p[2],g[1],g[2],gb,O,1,delay)
-  tr_idx <- c(which(equals_plus(c_board[,1],current_p[1]) & equals_plus(c_board[,2],current_p[2])))
-  for (t in 2:NROW(p_pos)) {
-    if (length(tr_idx) > delay) {
-      dat <- rbind(dat,list(c_board,tr_idx,g,dp))
-      c_board <- simulate_board(current_p[1],current_p[2],g[1],g[2],gb,O,t,delay)
-      tr_idx <- c(which(equals_plus(c_board[,1],current_p[1]) & equals_plus(c_board[,2],current_p[2])))
-    }
-    current_p <- c(p_pos[t,1],p_pos[t,2])
-    tr_idx <- c(tr_idx,which(equals_plus(c_board[,1],current_p[1]) & equals_plus(c_board[,2],current_p[2])))
-  }
-  if (length(tr_idx) > 0) {
-    dat <- rbind(dat,list(c_board,tr_idx,g,dp))
-  }
-  dat
-}
-
-process_simulated_data <- function(D,gb,delay=8) {
-  dat <- list()
-  for (d in 1:nrow(D)) {
-    p_pos <- D[[d,1]]
-    O <- D[[d,2]]
-    g <- D[[d,4]]
-    dat <- rbind(dat,process_simulated_run(p_pos,g,gb,O,delay))
-  }
-  dat
-}
-
-#step size in terms of units (e.g., step_size = 1 means each step is a 1 unit)
-map_global_trajectory <- function(p_df,gb,step_size = 1) {
-  half_step <- step_size/2
-  c_idx <- which(greater_equals_plus(p_df[1,1],(gb$cols - half_step)) & 
-                   lesser_equals_plus(p_df[1,1],(gb$cols + half_step)) &
-                   greater_equals_plus(p_df[1,2],(gb$rows - half_step)) & 
-                   lesser_equals_plus(p_df[1,2],(gb$rows + half_step)))[1]
-  tr_idx <- c(c_idx)
-  for (i in 2:nrow(p_df)) {
-    idx <- which(greater_equals_plus(p_df[i,1],(gb$cols - half_step)) & 
-                   lesser_equals_plus(p_df[i,1],(gb$cols + half_step)) &
-                   greater_equals_plus(p_df[i,2],(gb$rows - half_step)) & 
-                   lesser_equals_plus(p_df[i,2],(gb$rows + half_step)))[1]
-    if (length(idx) > 0) {
-      if (idx == c_idx) {
-        next
-      }
-      
-      c_idx <- idx
-      tr_idx <- c(tr_idx,c_idx)
-    }
-  }
-  tr_idx
-}
-
-
-
-#step size is in terms of game units and delay is in terms of step size (e.g., delay = 8, delayed by 8 steps)
-process_run_fixed_delay <- function(p_pos,g,gb,O,obs_st,omin=4.0,omax=16,delay=8,step_size=1) {
-  orig_x <- p_pos[1,1]
-  orig_y <- p_pos[1,2]
-  p_df <- data.frame(cols=(p_pos[,1]-orig_x),rows=(p_pos[,2]-orig_y),t=p_pos[,3])
-  gb_tr_idx <- map_global_trajectory(p_df,gb,step_size)
-  gx <- g[1] - orig_x
-  gy <- g[2] - orig_y
-  dat <- list()
-  dp <- create_uniform_default_policy(delay)
-  c_px <- gb[gb_tr_idx[1],1] 
-  c_py <- gb[gb_tr_idx[1],2]
-  o_df <- data.frame(cols=(O[,1]-orig_x),rows=(O[,2]-orig_y),angle=O[,3],t=O[,4],id=O[,5])
-  c_board <- create_board(c_px,c_py,gx,gy,o_df,obs_st,omin,omax,delay,step_size)
-  tr_idx <- c(which(equals_plus(c_board[,1],c_px) & equals_plus(c_board[,2],c_py)))
-  for (t in 2:length(gb_tr_idx)) {
-    if (length(tr_idx) > delay) {
-      dat <- rbind(dat,list(c_board,tr_idx,c(gx,gy),dp))
-      c_board <- create_board(c_px,c_py,gx,gy,o_df,obs_st,omin,omax,delay,step_size)
-      tr_idx <- c(which(equals_plus(c_board[,1],c_px) & equals_plus(c_board[,2],c_py)))
-    }
-    c_px <- gb[gb_tr_idx[t],1] 
-    c_py <- gb[gb_tr_idx[t],2]
-    tr_idx <- c(tr_idx,which(equals_plus(c_board[,1],c_px) & equals_plus(c_board[,2],c_py)))
-  }
-  if (length(tr_idx) > 0) {
-    dat <- rbind(dat,list(c_board,tr_idx,c(gx,gy),dp))
-  }
-  dat
 }
 
 compute_log_rl <- function(dat,b_1,b_2,lambda,plot_gb=FALSE) {
@@ -157,8 +68,7 @@ compute_grad_hess <- function(dat,b_1,b_2,lambda) {
     for(i in 1:length(z)) {
       u[i,which(dp[i,]>0)] <- dp[i,which(dp[i,]>0)]*z[which(dp[i,]>0)]/G[i]
     }
-    
-    
+
     a <- rep(0,length(z))
     b <- rep(0,length(z))
     
