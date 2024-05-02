3badf03bfd16963f0784b989ae1867615004cbd9
Loren Champlin <champlin@email.arizona.edu>
2024-05-02 12:23:28 -0700

    Processes multiple data runs

--- planners/lmdp/ioc_data.R
+++ planners/lmdp/ioc_data.R
@@ -1,25 +1,15 @@
 source("~/busy-beeway/planners/lmdp/ioc_graph.R")
 
-#step size is in terms of game units and delay is in terms of step size (e.g., delay = 8, delayed by 8 steps)
-process_run_fixed_delay <- function(p_pos,g,gb,O,obs_st,delay=8,sample_size=29) {
+process_session <- function(D,obs_st,delT=3,sample_size=100,rho=0.3) {
   dat <- list()
-  dp <- create_uniform_default_policy(delay)
-  c_px <- gb[gb_tr_idx[1],1] 
-  c_py <- gb[gb_tr_idx[1],2]
-  c_board <- create_board(c_px,c_py,gx,gy,o_df[which(o_df$t == 0),],obs_st,omin,omax,delay,time_step,pspeed)
-  tr_idx <- c(which(equals_plus(c_board[,1],c_px) & equals_plus(c_board[,2],c_py)))
-  for (t in 2:length(gb_tr_idx)) {
-    if (length(tr_idx) > delay) {
-      dat <- rbind(dat,list(c_board,tr_idx,c(gx,gy),dp))
-      c_board <- create_board(c_px,c_py,gx,gy,o_df[which(o_df$t == (t-1)),],obs_st,omin,omax,delay,time_step,pspeed)
-      tr_idx <- c(which(equals_plus(c_board[,1],c_px) & equals_plus(c_board[,2],c_py)))
-    }
-    c_px <- gb[gb_tr_idx[t],1] 
-    c_py <- gb[gb_tr_idx[t],2]
-    tr_idx <- c(tr_idx,which(equals_plus(c_board[,1],c_px) & equals_plus(c_board[,2],c_py)))
+  
+  for (i in 1:nrow(D)) {
+    p_df <- D[[i,1]]
+    O <- D[[i,2]]
+    g <- D[[i,4]]
+    s_dat <- create_state_space_data(p_df,g,O,obs_st,delT,sample_size,rho)
+    dat <- rbind(dat,list(s_dat))
   }
-  if (length(tr_idx) > 0) {
-    dat <- rbind(dat,list(c_board,tr_idx,c(gx,gy),dp))
-  }
+  
   dat
 }
