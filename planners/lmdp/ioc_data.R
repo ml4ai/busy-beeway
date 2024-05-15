acedf61857be56a1bcb5cc0f0087d016615b2c53
Loren Champlin <champlin@email.arizona.edu>
2024-05-15 10:28:07 -0700

    renamed function

--- planners/lmdp/ioc_data.R
+++ planners/lmdp/ioc_data.R
@@ -1,13 +1,13 @@
 source("~/busy-beeway/planners/lmdp/ioc_state_space.R")
 
-process_session <- function(D,obs_st,delT=3,sample_size=100,rho=0.3) {
+process_session <- function(D,obs_st,delT=3,sample_size=100,rho=0.3,normalize=FALSE) {
   dat <- list()
   
   for (i in 1:nrow(D)) {
     p_df <- D[[i,1]]
     O <- D[[i,2]]
     g <- D[[i,4]]
-    s_dat <- create_state_space_data(p_df,g,O,obs_st,delT,sample_size,rho)
+    s_dat <- create_state_space_data_bb(p_df,g,O,obs_st,delT,sample_size,rho,normalize)
     dat <- rbind(dat,list(s_dat))
   }
   
