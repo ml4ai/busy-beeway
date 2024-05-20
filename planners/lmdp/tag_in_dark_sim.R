c34850bab9e8796b716532ea4eb730b4e34b9c11
Loren Champlin <champlin@email.arizona.edu>
2024-05-20 11:10:30 -0700

    Renamed function

---
+++ planners/lmdp/tag_in_dark_sim.R
@@ -0,0 +1,150 @@
+source("~/busy-beeway/planners/lmdp/utility.R")
+source("~/busy-beeway/planners/lmdp/tid_state_space.R")
+source("~/busy-beeway/planners/lmdp/value_functions.R")
+
+move_targets <- function(O,P,grid_length) {
+  new_O <- NULL
+  for (o in 1:nrow(O)) {
+    if (O[o,3]) {
+      o_x <- O[o,1]
+      o_y <- O[o,2]
+      for (i in 1:2) {
+        can_stay <- !(P$x == o_x & P$y == o_y)
+        #1
+        can_up <- !(P$x == o_x & P$y == (o_y + 1)) & ((o_y + 1) <= grid_length)
+        #2
+        can_down <- !(P$x == o_x & P$y == (o_y - 1)) & ((o_y - 1) >= 1)
+        #3
+        can_left <- !(P$x == (o_x - 1) & P$y == o_y) & ((o_x - 1) >= 1)
+        #4
+        can_right <- !(P$x == (o_x + 1) & P$y == o_y) & ((o_x + 1) <= grid_length)
+        if (can_stay) {
+          if (!rbinom(1,1,0.5)) {
+            pos_moves <- which(c(can_up,can_down,can_left,can_right))
+            if (length(pos_moves) > 0) {
+              new_move <- switch(sample(pos_moves,1),
+                                 c(o_x,o_y + 1),
+                                 c(o_x,o_y - 1),
+                                 c(o_x - 1,o_y),
+                                 c(o_x + 1,o_y))
+              o_x <- new_move[1]
+              o_y <- new_move[2]
+            }
+          }
+        }
+        else {
+          pos_moves <- which(c(can_up,can_down,can_left,can_right))
+          if (length(pos_moves) > 0) {
+            new_move <- switch(sample(pos_moves,1),
+                               c(o_x,o_y + 1),
+                               c(o_x,o_y - 1),
+                               c(o_x - 1,o_y),
+                               c(o_x + 1,o_y))
+            o_x <- new_move[1]
+            o_y <- new_move[2]
+          }
+        }
+      }
+      new_O <- rbind(new_O,data.frame(x=o_x,y=o_y,color=O[o,3],t=O[o,4] + 1))
+    }
+    else {
+      can_stay <- !(P$x == O[o,1] & P$y == O[o,2])
+      #1
+      can_up <- !(P$x == O[o,1] & P$y == (O[o,2] + 1)) & ((O[o,2] + 1) <= grid_length)
+      #2
+      can_down <- !(P$x == O[o,1] & P$y == (O[o,2] - 1)) & ((O[o,2] - 1) >= 1)
+      #3
+      can_left <- !(P$x == (O[o,1] - 1) & P$y == O[o,2]) & ((O[o,1] - 1) >= 1)
+      #4
+      can_right <- !(P$x == (O[o,1] + 1) & P$y == O[o,2]) & ((O[o,1] + 1) <= grid_length)
+      if (can_stay) {
+        if (rbinom(1,1,0.5)) {
+          new_O <- rbind(new_O,data.frame(x=O[o,1],y=O[o,2],color=O[o,3],t=O[o,4]+1))
+        }
+        else {
+          pos_moves <- which(c(can_up,can_down,can_left,can_right))
+          if (length(pos_moves) == 0) {
+            new_O <- rbind(new_O,data.frame(x=O[o,1],y=O[o,2],color=O[o,3],t=O[o,4]+1))
+          }
+          else {
+            new_move <- switch(sample(pos_moves,1),
+                               c(O[o,1],O[o,2] + 1),
+                               c(O[o,1],O[o,2] - 1),
+                               c(O[o,1] - 1,O[o,2]),
+                               c(O[o,1] + 1,O[o,2]))
+            new_O <- rbind(new_O,data.frame(x=new_move[1],y=new_move[2],color=O[o,3],t=O[o,4]+1))
+          }
+        }
+      }
+      else {
+        pos_moves <- which(c(can_up,can_down,can_left,can_right))
+        if (length(pos_moves) == 0) {
+          new_O <- rbind(new_O,data.frame(x=O[o,1],y=O[o,2],color=O[o,3],t=O[o,4]+1))
+        }
+        else {
+          new_move <- switch(sample(pos_moves,1),
+                             c(O[o,1],O[o,2] + 1),
+                             c(O[o,1],O[o,2] - 1),
+                             c(O[o,1] - 1,O[o,2]),
+                             c(O[o,1] + 1,O[o,2]))
+          new_O <- rbind(new_O,data.frame(x=new_move[1],y=new_move[2],color=O[o,3],t=O[o,4]+1))
+        }
+      }
+    }
+  }
+  new_O
+}
+
+sim_session <- function(b1,
+                        b2,
+                        t_dat,
+                        delT=2,
+                        time_limit=30,
+                        grid_length=10,
+                        n_green=10,
+                        n_yellow=5) {
+  grid_seq <- 1:grid_length
+  colors <- c(rep(0,n_green),rep(1,n_yellow))
+  vf1 <- create_vf_tid(b1,b2)
+  grid <- expand.grid(x=grid_seq, y=grid_seq)
+  grid_dist <- as.matrix(dist(matrix(c(grid$x,grid$y),nrow(grid),2),
+                              method="manhattan",
+                              diag=TRUE,
+                              upper=TRUE))
+  # green = 0, yellow = 1
+  O <- data.frame(x = sample(grid_seq,length(colors),replace=TRUE),
+                  y = sample(grid_seq,length(colors),replace=TRUE), 
+                  color = colors,
+                  t=rep(0,length(colors)))
+  repeat {
+    px <- sample(grid_seq,1)
+    py <- sample(grid_seq,1)
+    if (!any(O$x == px & O$y == py)) {
+      break
+    }
+  }
+  
+  P <- data.frame(x=px,y=py,t=0)
+  for (t in 1:time_limit) {
+    new_O <- move_targets(O[which(O$t == (t-1)),],P[t,],grid_length)
+    temp_O <- rbind(O,new_O)
+    new_states <- create_state_space(c(P[t,1],P[t,2]),temp_O,grid,delT,t_dat,grid_dist,t)
+    v <- vf1(new_states)
+    z <- exp(-v)
+    max_z <- sample(which(equals_plus(z,max(z))),1)
+    new_P <- data.frame(x=new_states[max_z,1],y=new_states[max_z,2],t=t)
+    P <- rbind(P,new_P)
+    caught <- which(equals_plus(new_O$x,new_P[1,1]) & equals_plus(new_O$y,new_P[1,2]))
+    if (length(caught) > 0) {
+      caught <- sample(caught,1)
+      O <- rbind(O,new_O[-caught,])  
+    }
+    else {
+      O <- temp_O
+    }
+  }
+  green_left <- nrow(O[which(O$t == time_limit & O$color == 0),])
+  yellow_left <- nrow(O[which(O$t == time_limit & O$color == 1),])
+  score <- 5*(n_green - green_left) + 10*(n_yellow - yellow_left)
+  list(P,O,green_left,yellow_left,score)
+}
---
+++ planners/lmdp/tid_state_space.R
@@ -0,0 +1,134 @@
+move_target <- function(ox,oy,oc,grid_length) {
+  if (oc) {
+    o_x <- ox
+    o_y <- oy
+    for (i in 1:2) {
+      can_up <- (o_y + 1) <= grid_length
+      #2
+      can_down <- (o_y - 1) >= 1
+      #3
+      can_left <- (o_x - 1) >= 1
+      #4
+      can_right <- (o_x + 1) <= grid_length
+
+      if (!rbinom(1,1,0.5)) {
+        pos_moves <- which(c(can_up,can_down,can_left,can_right))
+        if (length(pos_moves) > 0) {
+          new_move <- switch(sample(pos_moves,1),
+                             c(o_x,o_y + 1),
+                             c(o_x,o_y - 1),
+                             c(o_x - 1,o_y),
+                             c(o_x + 1,o_y))
+          o_x <- new_move[1]
+          o_y <- new_move[2]
+        }
+      }
+    }
+    ox <- o_x
+    oy <- o_y
+  }
+  else {
+    #1
+    can_up <- (oy + 1) <= grid_length
+    #2
+    can_down <- (oy - 1) >= 1
+    #3
+    can_left <- (ox - 1) >= 1
+    #4
+    can_right <- (ox + 1) <= grid_length
+
+    if (!rbinom(1,1,0.5)) {
+      pos_moves <- which(c(can_up,can_down,can_left,can_right))
+      if (length(pos_moves) > 0) {
+        new_move <- switch(sample(pos_moves,1),
+                           c(ox,oy + 1),
+                           c(ox,oy - 1),
+                           c(ox - 1,oy),
+                           c(ox + 1,oy))
+        ox <- new_move[1]
+        oy <- new_move[2]
+      }
+    }
+  }
+  c(ox,oy)
+}
+
+create_t_dat <- function(delT,grid,samps) {
+  g_mat <- matrix(0,nrow(grid),nrow(grid))
+  y_mat <- g_mat
+  grid_length <- max(grid$x)
+  for (i in 1:nrow(grid)) {
+    for (n in 1:samps) {
+      g_move <- c(grid[i,1],grid[i,2])
+      y_move <- g_move
+      for (t in 0:delT) {
+        g_move <- move_target(g_move[1],g_move[2],0,grid_length)
+        y_move <- move_target(y_move[1],y_move[2],1,grid_length)
+      }
+      
+      g_match <- which(equals_plus(grid$x,g_move[1]) & equals_plus(grid$y,g_move[2]))
+      y_match <- which(equals_plus(grid$x,y_move[1]) & equals_plus(grid$y,y_move[2]))
+      
+      g_mat[i,g_match] <- g_mat[i,g_match] + 1
+      y_mat[i,y_match] <- y_mat[i,y_match] + 1
+    }
+  }
+  g_mat <- g_mat/rowSums(g_mat)
+  y_mat <- y_mat/rowSums(y_mat)
+  list(g_mat,y_mat)
+}
+
+create_state_space <- function(p,O,grid,delT,t_dat,grid_dist,t) {
+  o_t <- (t-1) - delT
+  O_t <- O[which(O$t == o_t),]
+  grid_length <- max(grid$x)
+  p_matches <- c()
+  if ((p[2] + 1) <= grid_length) {
+    p_matches <- c(p_matches,which(equals_plus(grid$x,p[1]) & equals_plus(grid$y,p[2] + 1)))
+  }
+  #2
+  if ((p[2] - 1) >= 1) {
+    p_matches <- c(p_matches,which(equals_plus(grid$x,p[1]) & equals_plus(grid$y,p[2] - 1)))
+  }
+  #3
+  if ((p[1] - 1) >= 1) {
+    p_matches <- c(p_matches,which(equals_plus(grid$x,p[1] - 1) & equals_plus(grid$y,p[2])))
+  }
+  #4
+  if ((p[1] + 1) <= grid_length) {
+    p_matches <- c(p_matches,which(equals_plus(grid$x,p[1] + 1) & equals_plus(grid$y,p[2])))
+  }
+  new_states <- NULL
+  if (nrow(O_t) > 0) {
+    for (n_p in p_matches) {
+      min_g_edist <- Inf
+      min_y_edist <- Inf
+      for (o in 1:nrow(O_t)) {
+        o_match <- which(equals_plus(grid$x,O_t[o,1]) & equals_plus(grid$y,O_t[o,2]))
+        if (O_t[o,3]) {
+          y_edist <- sum(grid_dist[n_p,]*t_dat[[2]][o_match,])
+          if (y_edist < min_y_edist) {
+            min_y_edist <- y_edist
+          }
+        }
+        else {
+          g_edist <- sum(grid_dist[n_p,]*t_dat[[1]][o_match,])
+          if (g_edist < min_g_edist) {
+            min_g_edist <- g_edist
+          }
+        }
+      }
+      new_states <- rbind(new_states,data.frame(x=grid[n_p,1],
+                                                y=grid[n_p,2],
+                                                min_g_edist=min_g_edist,
+                                                min_y_edist=min_y_edist))
+    }
+  }
+  else {
+    new_states <- rbind(new_states,data.frame(x=grid[p_matches,1],
+                                              y=grid[p_matches,2],
+                                              min_g_edist=rep(0,length(p_matches)),
+                                              min_y_edist=rep(0,length(p_matches))))
+  }
+  new_states
+}
