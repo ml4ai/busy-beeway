dccd056a0211bf3a95112843b392cbfa09004842
Loren Champlin <champlin@email.arizona.edu>
2024-05-21 14:50:51 -0700

    Processes mct data

---
+++ planners/lmdp/mct_data.R
@@ -0,0 +1,8 @@
+source("~/busy-beeway/planners/lmdp/mct_state_space.R")
+
+process_session <- function(d,p_dat,delT=2,bets=c(5,10,15,20,25),c=100) {
+  P <- d[[1]]
+  O <- d[[2]]
+  s_dat <- create_state_space_data(P,O,bets,delT,p_dat)
+  s_dat
+}
