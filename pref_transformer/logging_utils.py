bcd0334485635c08a84713a8251c98cbf85f0728
Loren Champlin <champlin@email.arizona.edu>
2024-08-06 15:03:04 -0700

    Renamed

--- pref_transformer/logging_utils.py
+++ pref_transformer/logging_utils.py
@@ -4,6 +4,7 @@ Based on rllab's logger.
 
 https://github.com/rll/rllab
 """
+
 from enum import Enum
 from contextlib import contextmanager
 import numpy as np
@@ -38,8 +39,9 @@ class TerminalTablePrinter(object):
 
     def refresh(self):
         import os
-        rows, columns = os.popen('stty size', 'r').read().split()
-        tabulars = self.tabulars[-(int(rows) - 3):]
+
+        rows, columns = os.popen("stty size", "r").read().split()
+        tabulars = self.tabulars[-(int(rows) - 3) :]
         sys.stdout.write("\x1b[2J\x1b[H")
         sys.stdout.write(tabulate(tabulars, self.headers))
         sys.stdout.write("\n")
@@ -48,15 +50,11 @@ class TerminalTablePrinter(object):
 class MyEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, type):
-            return {'$class': o.__module__ + "." + o.__name__}
+            return {"$class": o.__module__ + "." + o.__name__}
         elif isinstance(o, Enum):
-            return {
-                '$enum': o.__module__ + "." + o.__class__.__name__ + '.' + o.name
-            }
+            return {"$enum": o.__module__ + "." + o.__class__.__name__ + "." + o.name}
         elif callable(o):
-            return {
-                '$function': o.__module__ + "." + o.__name__
-            }
+            return {"$function": o.__module__ + "." + o.__name__}
         return json.JSONEncoder.default(self, o)
 
 
@@ -73,10 +71,10 @@ def mkdir_p(path):
 class Logger(object):
     def __init__(self):
         self._prefixes = []
-        self._prefix_str = ''
+        self._prefix_str = ""
 
         self._tabular_prefixes = []
-        self._tabular_prefix_str = ''
+        self._tabular_prefix_str = ""
 
         self._tabular = []
 
@@ -88,7 +86,7 @@ class Logger(object):
         self._tabular_header_written = set()
 
         self._snapshot_dir = None
-        self._snapshot_mode = 'all'
+        self._snapshot_mode = "all"
         self._snapshot_gap = 1
 
         self._log_tabular_only = False
@@ -98,7 +96,7 @@ class Logger(object):
     def reset(self):
         self.__init__()
 
-    def _add_output(self, file_name, arr, fds, mode='a'):
+    def _add_output(self, file_name, arr, fds, mode="a"):
         if file_name not in arr:
             mkdir_p(os.path.dirname(file_name))
             arr.append(file_name)
@@ -112,11 +110,10 @@ class Logger(object):
 
     def push_prefix(self, prefix):
         self._prefixes.append(prefix)
-        self._prefix_str = ''.join(self._prefixes)
+        self._prefix_str = "".join(self._prefixes)
 
     def add_text_output(self, file_name):
-        self._add_output(file_name, self._text_outputs, self._text_fds,
-                         mode='a')
+        self._add_output(file_name, self._text_outputs, self._text_fds, mode="a")
 
     def remove_text_output(self, file_name):
         self._remove_output(file_name, self._text_outputs, self._text_fds)
@@ -124,8 +121,7 @@ class Logger(object):
     def add_tabular_output(self, file_name, relative_to_snapshot_dir=False):
         if relative_to_snapshot_dir:
             file_name = osp.join(self._snapshot_dir, file_name)
-        self._add_output(file_name, self._tabular_outputs, self._tabular_fds,
-                         mode='w')
+        self._add_output(file_name, self._tabular_outputs, self._tabular_fds, mode="w")
 
     def remove_tabular_output(self, file_name, relative_to_snapshot_dir=False):
         if relative_to_snapshot_dir:
@@ -137,16 +133,22 @@ class Logger(object):
     def set_snapshot_dir(self, dir_name):
         self._snapshot_dir = dir_name
 
-    def get_snapshot_dir(self, ):
+    def get_snapshot_dir(
+        self,
+    ):
         return self._snapshot_dir
 
-    def get_snapshot_mode(self, ):
+    def get_snapshot_mode(
+        self,
+    ):
         return self._snapshot_mode
 
     def set_snapshot_mode(self, mode):
         self._snapshot_mode = mode
 
-    def get_snapshot_gap(self, ):
+    def get_snapshot_gap(
+        self,
+    ):
         return self._snapshot_gap
 
     def set_snapshot_gap(self, gap):
@@ -155,7 +157,9 @@ class Logger(object):
     def set_log_tabular_only(self, log_tabular_only):
         self._log_tabular_only = log_tabular_only
 
-    def get_log_tabular_only(self, ):
+    def get_log_tabular_only(
+        self,
+    ):
         return self._log_tabular_only
 
     def log(self, s, with_prefix=True, with_timestamp=True):
@@ -164,13 +168,13 @@ class Logger(object):
             out = self._prefix_str + out
         if with_timestamp:
             now = datetime.datetime.now(dateutil.tz.tzlocal())
-            timestamp = now.strftime('%Y-%m-%d %H:%M:%S.%f %Z')
+            timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f %Z")
             out = "%s | %s" % (timestamp, out)
         if not self._log_tabular_only:
             # Also log to stdout
             print(out)
             for fd in list(self._text_fds.values()):
-                fd.write(out + '\n')
+                fd.write(out + "\n")
                 fd.flush()
             sys.stdout.flush()
 
@@ -187,32 +191,39 @@ class Logger(object):
 
     def push_tabular_prefix(self, key):
         self._tabular_prefixes.append(key)
-        self._tabular_prefix_str = ''.join(self._tabular_prefixes)
+        self._tabular_prefix_str = "".join(self._tabular_prefixes)
 
-    def pop_tabular_prefix(self, ):
+    def pop_tabular_prefix(
+        self,
+    ):
         del self._tabular_prefixes[-1]
-        self._tabular_prefix_str = ''.join(self._tabular_prefixes)
+        self._tabular_prefix_str = "".join(self._tabular_prefixes)
 
-    def save_extra_data(self, data, file_name='extra_data.pkl', mode='joblib'):
+    def save_extra_data(self, data, file_name="extra_data.pkl", mode="joblib"):
         """
         Data saved here will always override the last entry
 
         :param data: Something pickle'able.
         """
         file_name = osp.join(self._snapshot_dir, file_name)
-        if mode == 'joblib':
+        if mode == "joblib":
             import joblib
+
             joblib.dump(data, file_name, compress=3)
-        elif mode == 'pickle':
+        elif mode == "pickle":
             pickle.dump(data, open(file_name, "wb"))
         else:
             raise ValueError("Invalid mode: {}".format(mode))
         return file_name
 
-    def get_table_dict(self, ):
+    def get_table_dict(
+        self,
+    ):
         return dict(self._tabular)
 
-    def get_table_key_set(self, ):
+    def get_table_key_set(
+        self,
+    ):
         return set(key for key, value in self._tabular)
 
     @contextmanager
@@ -234,8 +245,8 @@ class Logger(object):
         with open(log_file, "w") as f:
             json.dump(variant_data, f, indent=2, sort_keys=True, cls=MyEncoder)
 
-    def record_tabular_misc_stat(self, key, values, placement='back'):
-        if placement == 'front':
+    def record_tabular_misc_stat(self, key, values, placement="back"):
+        if placement == "front":
             prefix = ""
             suffix = key
         else:
@@ -260,25 +271,29 @@ class Logger(object):
             if self._log_tabular_only:
                 self.table_printer.print_tabular(self._tabular)
             else:
-                for line in tabulate(self._tabular).split('\n'):
+                for line in tabulate(self._tabular).split("\n"):
                     self.log(line, *args, **kwargs)
             tabular_dict = dict(self._tabular)
             # Also write to the csv files
             # This assumes that the keys in each iteration won't change!
             for tabular_fd in list(self._tabular_fds.values()):
-                writer = csv.DictWriter(tabular_fd,
-                                        fieldnames=list(tabular_dict.keys()))
+                writer = csv.DictWriter(
+                    tabular_fd, fieldnames=list(tabular_dict.keys())
+                )
                 if wh or (
-                        wh is None and tabular_fd not in self._tabular_header_written):
+                    wh is None and tabular_fd not in self._tabular_header_written
+                ):
                     writer.writeheader()
                     self._tabular_header_written.add(tabular_fd)
                 writer.writerow(tabular_dict)
                 tabular_fd.flush()
             del self._tabular[:]
 
-    def pop_prefix(self, ):
+    def pop_prefix(
+        self,
+    ):
         del self._prefixes[-1]
-        self._prefix_str = ''.join(self._prefixes)
+        self._prefix_str = "".join(self._prefixes)
 
 
 def safe_json(data):
@@ -319,16 +334,16 @@ def create_exp_name(exp_prefix, exp_id=0, seed=0):
     :return:
     """
     now = datetime.datetime.now(dateutil.tz.tzlocal())
-    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+    timestamp = now.strftime("%Y_%m_%d_%H_%M_%S")
     return "%s_%s-s-%d--%s" % (exp_prefix, timestamp, seed, str(exp_id))
 
 
 def create_log_dir(
-        exp_prefix,
-        exp_id=0,
-        seed=0,
-        base_log_dir=None,
-        include_exp_prefix_sub_dir=True,
+    exp_prefix,
+    exp_id=0,
+    seed=0,
+    base_log_dir=None,
+    include_exp_prefix_sub_dir=True,
 ):
     """
     Creates and returns a unique log directory.
@@ -340,14 +355,13 @@ def create_log_dir(
     :param base_log_dir: The directory where all log should be saved.
     :return:
     """
-    exp_name = create_exp_name(exp_prefix, exp_id=exp_id,
-                               seed=seed)
+    exp_name = create_exp_name(exp_prefix, exp_id=exp_id, seed=seed)
     if base_log_dir is None:
         base_log_dir = conf.LOCAL_LOG_DIR
-#     if include_exp_prefix_sub_dir:
-#         log_dir = osp.join(base_log_dir, exp_prefix.replace("_", "-"), exp_name)
-#     else:
-#         log_dir = osp.join(base_log_dir, exp_name)
+    #     if include_exp_prefix_sub_dir:
+    #         log_dir = osp.join(base_log_dir, exp_prefix.replace("_", "-"), exp_name)
+    #     else:
+    #         log_dir = osp.join(base_log_dir, exp_name)
     log_dir = base_log_dir
     if osp.exists(log_dir):
         print("WARNING: Log directory already exists {}".format(log_dir))
@@ -356,16 +370,16 @@ def create_log_dir(
 
 
 def setup_logger(
-        exp_prefix="default",
-        variant=None,
-        text_log_file="debug.log",
-        variant_log_file="variant.json",
-        tabular_log_file="progress.csv",
-        snapshot_mode="last",
-        snapshot_gap=1,
-        log_tabular_only=False,
-        base_log_dir=None,
-        **create_log_dir_kwargs
+    exp_prefix="default",
+    variant=None,
+    text_log_file="debug.log",
+    variant_log_file="variant.json",
+    tabular_log_file="progress.csv",
+    snapshot_mode="last",
+    snapshot_gap=1,
+    log_tabular_only=False,
+    base_log_dir=None,
+    **create_log_dir_kwargs
 ):
     """
     Set up logger to have some reasonable default settings.
