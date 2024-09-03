5d9f4d732788dd2fcac674faf097111ebd196247
Loren Champlin <champlin@email.arizona.edu>
2024-09-03 13:11:45 -0700

    Formatted code

--- transformers/utils.py
+++ transformers/utils.py
@@ -5,29 +5,33 @@ import os
 from jax_utils import init_rng
 import numpy as np
 
+
 def set_random_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
     init_rng(seed)
 
+# Memory Mapped Arrays get loaded into physical memory here
 def index_batch(batch, indices):
     indexed = {}
     for key in batch.keys():
         indexed[key] = batch[key][indices, ...]
     return indexed
 
+
 def prefix_metrics(metrics, prefix):
-    return {
-        '{}/{}'.format(prefix, key): value for key, value in metrics.items()
-    }
+    return {"{}/{}".format(prefix, key): value for key, value in metrics.items()}
+
 
 def save_pickle(obj, filename, output_dir):
-    with open(os.path.join(output_dir, filename), 'wb') as fout:
+    with open(os.path.join(output_dir, filename), "wb") as fout:
         pickle.dump(obj, fout)
 
+
 def load_pickle(filename):
-    with open(os.path.expanduser(filename), 'rb') as fin:
+    with open(os.path.expanduser(filename), "rb") as fin:
         return pickle.load(fin)
+
 
 class Timer(object):
 
