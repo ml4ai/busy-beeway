70370d5dd92c8ba907afa65a8ec298f7fafea64b
Loren Champlin <champlin@email.arizona.edu>
2024-09-03 13:10:07 -0700

    Setup commandline flags

---
+++ transformers/compile_preference_data.py
@@ -0,0 +1,178 @@
+import argparse
+import sys
+
+from argformat import StructuredFormatter
+
+from bb_data_loading import load_participant_data, load_participant_data_p
+from data_utils import (
+    compute_features,
+    compute_features_p,
+    create_preference_data,
+    load_features_from_parquet,
+)
+from replayer import generate_stats, goal_only_replay, goal_only_replay_p, load_stats
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description="Compiles preference feature data into a digestible form for Preference Transformer",
+        formatter_class=StructuredFormatter,
+    )
+    parser.add_argument(
+        "p_id",
+        metavar="PID",
+        type=str,
+        help="Participant ID",
+    )
+    parser.add_argument(
+        "-d",
+        "--data_dir",
+        type=str,
+        default="~/busy-beeway/data/game_data",
+        help="Data Directory.",
+    )
+    parser.add_argument(
+        "-f",
+        "--fill_size",
+        type=int,
+        help="Padding size for sequences. \nUses max sequence length by default.",
+    )
+    parser.add_argument(
+        "-a",
+        "--arc_sweep",
+        type=int,
+        default=[10, 360, 10],
+        nargs=3,
+        help="Settings for arc sizes for feature computation. \n(Starting Size, Ending Size, Increment Size).",
+    )
+    parser.add_argument(
+        "-s",
+        "--seed",
+        type=int,
+        default=13138787,
+        help="Random seed used throughout the program.",
+    )
+    parser.add_argument(
+        "-p",
+        "--parallel",
+        action="store_true",
+        help="Attempts to run certain processes in parallel.",
+    )
+    parser.add_argument(
+        "-c",
+        "--cache_features",
+        action="store_true",
+        help="Caches feature data.",
+    )
+    parser.add_argument(
+        "-k",
+        "--cache_stats",
+        action="store_true",
+        help="Caches movement statistics (recommended).",
+    )
+    parser.add_argument(
+        "-l",
+        "--load_features",
+        type=str,
+        default=None,
+        help="Tries to load feature data from \ndirectories 'f_save' and 'rf_save' \nwithin the given path.",
+    )
+    parser.add_argument(
+        "-j",
+        "--load_stats",
+        type=str,
+        default=None,
+        help="Tries to load movement statistics \nfrom 'p_stats.npy' within the \ngiven path (recommended).",
+    )
+    parser.add_argument(
+        "-z",
+        "--npz_save",
+        action="store_true",
+        help="Saves compiled preference data as single .npz file.",
+    )
+    args = parser.parse_args(argv)
+    parallel = args.parallel
+    path = args.data_dir
+    p_id = args.p_id
+    fill_size = args.fill_size
+    arc_sweep = tuple(args.arc_sweep)
+    seed = args.seed
+    load_features = args.load_features
+    load_stats = args.load_stats
+    if args.cache_stats:
+        save_p_stats = "p_stats.npy"
+    else:
+        save_p_stats = None
+    if args.cache_features:
+        save_f = "f_save"
+        save_rf = "rf_save"
+
+    if args.npz_save:
+        save_pref = f"preference_data/{p_id}.npz"
+    else:
+        save_pref = f"preference_data/{p_id}"
+    pd_not_loaded = True
+    if parallel:
+        try:
+            if load_features:
+                RF = load_features_from_parquet("cache/rf_save")
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            try:
+                if load_stats:
+                    stats = load_stats("cache/p_stats.npy")
+                else:
+                    raise FileNotFoundError
+            except FileNotFoundError:
+                CD = load_participant_data_p(p_id=p_id, path=path, control=3)
+                stats = generate_stats(CD, save_data=save_p_stats)
+            D = load_participant_data_p(p_id=p_id, path=path)
+            pd_not_loaded = False
+            RD = goal_only_replay_p(D, stats, seed=seed)
+            RF = compute_features_p(RD, arc_sweep, save_dir=save_rf)
+        try:
+            if load_features:
+                F = load_features_from_parquet("cache/f_save")
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            if pd_not_loaded:
+                D = load_participant_data_p(p_id=p_id, path=path)
+            F = compute_features_p(D, arc_sweep, save_dir=save_f)
+        create_preference_data(RF, F, fill_size=fill_size, save_data=save_pref)
+        sys.exit(0)
+
+    try:
+        if load_features:
+            RF = load_features_from_parquet("cache/rf_save")
+        else:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        try:
+            if load_stats:
+                stats = load_stats("cache/p_stats.npy")
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            CD = load_participant_data(p_id=p_id, path=path, control=3)
+            stats = generate_stats(CD, save_data=save_p_stats)
+        D = load_participant_data(p_id=p_id, path=path)
+        pd_not_loaded = False
+        RD = goal_only_replay(D, stats, seed=seed)
+        RF = compute_features(RD, arc_sweep, save_dir=save_rf)
+    try:
+        if load_features:
+            F = load_features_from_parquet("cache/f_save")
+        else:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        if pd_not_loaded:
+            D = load_participant_data(p_id=p_id, path=path)
+        F = compute_features(D, arc_sweep, save_dir=save_f)
+    create_preference_data(RF, F, fill_size=fill_size, save_data=save_pref)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- transformers/run_pt_training.py
+++ transformers/run_pt_training.py
@@ -1,4 +1,5 @@
-import getopt
+import argparse
+from argformat import StructuredFormatter
 import sys
 
 import numpy as np
@@ -16,104 +17,83 @@ from train_model import train_pt
 
 
 def main(argv):
-    parallel = False
+    parser = argparse.ArgumentParser(
+        description="Runs Preference Transformer Training Algorithm given a compiled dataset (in jax.numpy.array form)",
+        formatter_class=StructuredFormatter,
+    )
+    parser.add_argument(
+        "data",
+        metavar="D",
+        type=str,
+        help="Location of the data. \nA single .npz file or a directory \ncontaining several .npy files.",
+    )
+    parser.add_argument(
+        "-t",
+        "--training_split",
+        type=float,
+        default=0.7,
+        help="Percentage of training data.",
+    )
+    parser.add_argument(
+        "-b",
+        "--batch_size",
+        type=int,
+        default=64,
+        help="Batch size. Powers of 2 work best.",
+    )
+    parser.add_argument(
+        "-e",
+        "--eval_period",
+        type=int,
+        default=1,
+        help="Period in which the validation set is ran and evaluated.",
+    )
+    parser.add_argument(
+        "-n", "--n_epochs", type=int, default=10, help="Number of training epochs."
+    )
+    parser.add_argument(
+        "-s",
+        "--seeds",
+        type=int,
+        default=13138787,
+        help="Random seed used throughout the program.",
+    )
+    parser.add_argument(
+        "-c",
+        "--cpu",
+        type=int,
+        default=0,
+        help="CPU ID (0 by default) for data loading. \nSetting to -1 lets Jax use a default device \n(a GPU if available).",
+    )
+    parser.add_argument(
+        "-l",
+        "--learning_rate",
+        type=float,
+        default=[0.0, 1e-4, 0.0],
+        nargs=3,
+        help="Learning Rate parameters passed to optimizer. \nIt uses a Cosine Decay Schedule with warmup steps, \nso this option requires 3 arguments \n(initial learning rate, peak learning rate, end learning rate).",
+    )
+    args = parser.parse_args(argv)
+    load_data = args.data
+
+    train_split = args.training_split
+    batch_size = args.batch_size
+    eval_period = args.eval_period
+    n_epochs = args.n_epochs
+    seed = args.seed
+    cpu = args.cpu
+    learing_rate = args.learning_rate
+    init_value = learing_rate[0]
+    peak_value = learing_rate[1]
+    end_value = learing_rate[2]
+    if cpu < 0:
+        cpu = None
     try:
-        opts, args = getopt.getopt(argv, "hp", ["help,parallel"])
-    except getopt.GetoptError:
-        print("usage: train_pt.py [-p --parallel]")
-        print("Options:")
-        print("-h --help        : Prints the current help message")
-        print(
-            "-p --parallel    : Runs compute_features functions using multiprocessing"
+        P = load_preference_data(load_data, sep_files=True, mmap_mode="r", cpu=cpu)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"{load_data} not found, try running compile_preference_data.py first!"
         )
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print("usage: train_pt.py [-p --parallel]")
-            print("Options:")
-            print("-h --help        : Prints the current help message")
-            print(
-                "-p --parallel    : Runs compute_features functions using multiprocessing"
-            )
-            sys.exit(0)
-        elif opt in ("-p", "--parallel"):
-            parallel = True
-
-    path = "~/busy-beeway/data/game_data"
-    p_id = "auto-1ba807eecf3cf284"
-
-    fill_size = None
-    train_split = 23 / 28
-    batch_size = 64
-    eval_period=1
-    n_epochs = 50
-    arc_sweep = (10, 360, 10)
-    seed = 31673
-
-    pd_not_loaded = True
-    if parallel:
-        try:
-            P = load_preference_data(f"~/busy-beeway/transformers/{p_id}",sep_files=True,mmap_mode='r+',cpu=0)
-        except FileNotFoundError:
-            try:
-                RF = load_features_from_parquet("~/busy-beeway/transformers/rf_save")
-            except FileNotFoundError:
-                try:
-                    stats = load_stats("~/busy-beeway/transformers/p_stats.npy")
-                except FileNotFoundError:
-                    CD = load_participant_data_p(p_id=p_id, path=path, control=3)
-                    stats = generate_stats(
-                        CD, save_data="~/busy-beeway/transformers/p_stats.npy"
-                    )
-                D = load_participant_data_p(p_id=p_id, path=path)
-                pd_not_loaded = False
-                RD = goal_only_replay_p(D, stats, seed=seed)
-                RF = compute_features_p(
-                    RD, arc_sweep, save_dir="~/busy-beeway/transformers/rf_save"
-                )
-            try:
-                F = load_features_from_parquet("~/busy-beeway/transformers/f_save")
-            except FileNotFoundError:
-                if pd_not_loaded:
-                    D = load_participant_data_p(p_id=p_id, path=path)
-                F = compute_features_p(
-                    D, arc_sweep, save_dir="~/busy-beeway/transformers/f_save"
-                )
-            P = create_preference_data(
-                RF, F, fill_size=fill_size, save_data=f"{p_id}.npz"
-            )
-    else:
-        try:
-            P = load_preference_data(f"~/busy-beeway/transformers/{p_id}",sep_files=True,mmap_mode='r+',cpu=0)
-        except FileNotFoundError:
-            try:
-                RF = load_features_from_parquet("~/busy-beeway/transformers/rf_save")
-            except FileNotFoundError:
-                try:
-                    stats = load_stats("~/busy-beeway/transformers/p_stats.npy")
-                except FileNotFoundError:
-                    CD = load_participant_data(p_id=p_id, path=path, control=3)
-                    stats = generate_stats(
-                        CD, save_data="~/busy-beeway/transformers/p_stats.npy"
-                    )
-                D = load_participant_data(p_id=p_id, path=path)
-                pd_not_loaded = False
-                RD = goal_only_replay(D, stats, seed=seed)
-                RF = compute_features(
-                    RD, arc_sweep, save_dir="~/busy-beeway/transformers/rf_save"
-                )
-            try:
-                F = load_features_from_parquet("~/busy-beeway/transformers/f_save")
-            except FileNotFoundError:
-                if pd_not_loaded:
-                    D = load_participant_data(p_id=p_id, path=path)
-                F = compute_features(
-                    D, arc_sweep, save_dir="~/busy-beeway/transformers/f_save"
-                )
-
-            P = create_preference_data(
-                RF, F, fill_size=fill_size, save_data=f"{p_id}.npz"
-            )
 
     rng = np.random.default_rng(seed)
     p_size = P["observations"].shape[0]
@@ -130,7 +110,15 @@ def main(argv):
     if isinstance(P, np.lib.npyio.NpzFile):
         P.close()
     train_pt(
-        training_data, test_data, batch_size=batch_size, n_epochs=n_epochs, eval_period=eval_period,seed=seed
+        training_data,
+        test_data,
+        batch_size=batch_size,
+        n_epochs=n_epochs,
+        eval_period=eval_period,
+        seed=seed,
+        init_value=init_value,
+        peak_value=peak_value,
+        end_value=end_value,
     )
     sys.exit(0)
 
