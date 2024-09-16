64361c7426e9d2ed8b9f4c142075542dfd5ae1c8
Loren Champlin <champlin@email.arizona.edu>
2024-09-15 22:31:32 -0700

    Fixed h5py pickle error

--- transformers/data_utils/data_loader.py
+++ transformers/data_utils/data_loader.py
@@ -7,29 +7,35 @@ class Pref_H5Dataset(torch.utils.data.Dataset):
 
     def __init__(self, file_path):
         super(Pref_H5Dataset, self).__init__()
-        h5_file = h5py.File(file_path, "r")
-        self.observations = h5_file["observations"]
-        self.timesteps = h5_file["timesteps"]
-        self.attn_mask = h5_file["attn_mask"]
-        self.observations_2 = h5_file["observations_2"]
-        self.timesteps_2 = h5_file["timesteps_2"]
-        self.attn_mask_2 = h5_file["attn_mask_2"]
-        self.labels = h5_file["labels"]
+        self.file_path = file_path
+        with h5py.File(self.file_path, "r") as f:
+            self.shape = f["observations"].shape
+
+    def open_hdf5(self):
+        self.h5_file = h5py.File(self.file_path, "r")
+        self.observations = self.h5_file["observations"]
+        self.timesteps = self.h5_file["timesteps"]
+        self.attn_mask = self.h5_file["attn_mask"]
+        self.observations_2 = self.h5_file["observations_2"]
+        self.timesteps_2 = self.h5_file["timesteps_2"]
+        self.attn_mask_2 = self.h5_file["attn_mask_2"]
+        self.labels = self.h5_file["labels"]
 
     def __getitem__(self, index):
-
+        if not hasattr(self, "h5_file"):
+            self.open_hdf5()
         return (
             np.asarray(self.observations[index, ...], dtype=np.float32),
-            np.asarray(self.timesteps[index, ...],dtype=np.float32),
-            np.asarray(self.attn_mask[index, ...],dtype=np.float32),
-            np.asarray(self.observations_2[index, ...],dtype=np.float32),
-            np.asarray(self.timesteps_2[index, ...],dtype=np.float32),
-            np.asarray(self.attn_mask_2[index, ...],dtype=np.float32),
+            np.asarray(self.timesteps[index, ...], dtype=np.float32),
+            np.asarray(self.attn_mask[index, ...], dtype=np.float32),
+            np.asarray(self.observations_2[index, ...], dtype=np.float32),
+            np.asarray(self.timesteps_2[index, ...], dtype=np.float32),
+            np.asarray(self.attn_mask_2[index, ...], dtype=np.float32),
             np.asarray(self.labels[index], dtype=np.float32),
         )
 
     def __len__(self):
-        return self.observations.shape[0]
+        return self.shape[0]
 
     def obs_shape(self):
-        return self.observations.shape
+        return self.shape
