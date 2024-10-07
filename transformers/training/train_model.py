011b294a9b4b0c8a44ce32ba7db36432ed39cb28
Loren Champlin <champlin@email.arizona.edu>
2024-10-06 21:50:06 -0700

    added maml training

--- transformers/training/train_model.py
+++ transformers/training/train_model.py
@@ -3,11 +3,16 @@ import os.path as osp
 import jax
 import jax.numpy as jnp
 import numpy as np
-from flax.training.early_stopping import EarlyStopping
 import torch
+from flax.training.early_stopping import EarlyStopping
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
+from transformers.data_utils.data_loader import (
+    FewShotBatchSampler,
+    participant_train_val_test_split,
+    process_p_batch,
+)
 from transformers.models.intervention_mlp import MLP
 from transformers.models.pref_transformer import PT
 from transformers.training.jax_utils import batch_to_jax
@@ -99,7 +104,7 @@ def train_pt(
         decay_steps=kwargs.get("decay_steps", int(n_epochs * interval)),
         end_value=kwargs.get("end_value", 0),
     )
-    early_stop = EarlyStopping(min_delta=1e-3, patience=10)
+    early_stop = EarlyStopping(min_delta=0, patience=0)
     c_best_epoch = np.nan
     c_criteria_key = np.nan
     for epoch, (s_key, t_key, e_key) in enumerate(
@@ -197,6 +202,193 @@ def train_pt(
         logger.dump_tabular(with_prefix=False, with_timestamp=False)
     if save_model:
         save_data = {"model": model, "epoch": epoch}
+        save_pickle(save_data, "model.pkl", save_dir)
+
+
+def train_mamlpt(
+    data,
+    seed,
+    train_val_test_split=(70, 5, 4),
+    N_way=5,
+    K_shot=4,
+    num_workers=2,
+    n_epochs=50,
+    inner_epochs=1,
+    eval_period=1,
+    do_early_stop=False,
+    criteria_key="eval_loss",
+    save_dir="~/busy-beeway/transformers/logs",
+    save_model=True,
+    **kwargs,
+):
+
+    save_dir = osp.expanduser(save_dir)
+    setup_logger(
+        variant=None,
+        seed=seed,
+        base_log_dir=save_dir,
+        include_exp_prefix_sub_dir=False,
+    )
+
+    _, query_len, observation_dim = data.obs_shape()
+    max_episode_length = data.max_episode_length()
+    rng_key = jax.random.PRNGKey(seed)
+    rng_key, rng_subkey = jax.random.split(rng_key, 2)
+    gen1 = torch.Generator().manual_seed(int(rng_subkey[0]))
+    gen2 = torch.Generator().manual_seed(int(rng_subkey[1]))
+    train_n, val_n, test_n = train_val_test_split
+    training, val, test = participant_train_val_test_split(
+        data, train_n, val_n, test_n, gen=gen1
+    )
+    training_data, training_p_idx = training
+    validation_data, validation_p_idx = val
+    # The list of test candidates gets pickled, so that a test set can be built later.
+    _, test_p_idx = test
+
+    training_data_loader = DataLoader(
+        training_data,
+        batch_sampler=FewShotBatchSampler(
+            training_p_idx,
+            include_query=True,
+            N_way=N_way,
+            K_shot=K_shot,
+            shuffle=True,
+            gen=gen2,
+        ),
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    validation_data_loader = DataLoader(
+        validation_data,
+        batch_sampler=FewShotBatchSampler(
+            validation_p_idx,
+            include_query=True,
+            N_way=N_way,
+            K_shot=K_shot,
+            shuffle=False,
+        ),
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+
+    interval = len(training_data_loader)
+    eval_interval = len(validation_data_loader)
+    max_pos = 512
+    while query_len > max_pos:
+        max_pos *= 2
+    embd_dim = kwargs.get("embd_dim", 256)
+    trans = PT(
+        observation_dim=observation_dim,
+        max_episode_steps=kwargs.get("max_episode_steps", max_episode_length),
+        embd_dim=embd_dim,
+        pref_attn_embd_dim=kwargs.get("pref_attn_embd_dim", embd_dim),
+        num_heads=kwargs.get("num_heads", 4),
+        attn_dropout=kwargs.get("attn_dropout", 0.1),
+        resid_dropout=kwargs.get("resid_dropout", 0.1),
+        intermediate_dim=kwargs.get("intermediate_dim", 4 * embd_dim),
+        num_layers=kwargs.get("num_layers", 1),
+        embd_dropout=kwargs.get("embd_dropout", 0.1),
+        max_pos=kwargs.get("max_pos", max_pos),
+        eps=kwargs.get("eps", 0.1),
+    )
+
+    rng_subkey1, rng_subkey2, rng_subkey3 = jax.random.split(rng_key, 3)
+    model = MAMLPTTrainer(
+        trans,
+        inner_epochs,
+        rng_subkey1,
+        rng_subkey2,
+        init_value=kwargs.get("init_value", 0),
+        peak_value=kwargs.get("peak_value", 1e-4),
+        warmup_steps=kwargs.get("warmup_steps", int(n_epochs * interval * 0.1)),
+        decay_steps=kwargs.get("decay_steps", int(n_epochs * interval)),
+        end_value=kwargs.get("end_value", 0),
+    )
+    early_stop = EarlyStopping(min_delta=0, patience=0)
+    c_best_epoch = np.nan
+    c_criteria_key = np.nan
+    for epoch, (s_key, t_key, e_key) in enumerate(
+        jax.random.split(rng_subkey3, (n_epochs + 1, 3))
+    ):
+        metrics = {
+            "epoch": epoch,
+            "train_time": np.nan,
+            "training_loss": [],
+            "training_acc": [],
+            "eval_loss": [],
+            "eval_acc": [],
+            "best_epoch": c_best_epoch,
+            f"{criteria_key}_best": c_criteria_key,
+        }
+        if epoch:
+            with Timer() as train_timer:
+                t_keys = jax.random.split(t_key, interval)
+                for i, t_data in tqdm(
+                    enumerate(training_data_loader),
+                    total=interval,
+                    desc=f"Training Epoch {epoch}",
+                ):
+                    batch = batch_to_jax(process_p_batch(t_data, N_way, K_shot))
+                    for key, val in model.train(batch, t_keys[i]).items():
+                        metrics[key].append(val)
+            metrics["train_time"] = train_timer()
+        else:
+            # for using early stopping with train loss.
+            metrics["training_loss"] = np.nan
+
+        # eval phase
+        if epoch % eval_period == 0:
+            e_keys = jax.random.split(e_key, eval_interval)
+            for j, e_data in tqdm(
+                enumerate(test_data_loader),
+                total=eval_interval,
+                desc=f"Evaluation Epoch {epoch}",
+            ):
+                batch = batch_to_jax(process_p_batch(e_data, N_way, K_shot))
+                for key, val in model.evaluation(batch, e_keys[j]).items():
+                    metrics[key].append(val)
+            criteria = np.mean(metrics[criteria_key])
+            early_stop = early_stop.update(criteria)
+            if early_stop.should_stop and do_early_stop:
+                for key, val in metrics.items():
+                    if isinstance(val, list):
+                        if len(val):
+                            metrics[key] = np.mean(val)
+                        else:
+                            metrics[key] = np.nan
+                logger.record_dict(metrics)
+                logger.dump_tabular(with_prefix=False, with_timestamp=False)
+                print("Met early stopping criteria, breaking...")
+                break
+            elif epoch > 0 and early_stop.has_improved:
+                c_best_epoch = epoch
+                c_criteria_key = criteria
+                metrics["best_epoch"] = c_best_epoch
+                metrics[f"{criteria_key}_best"] = c_criteria_key
+                save_data = {
+                    "model": model,
+                    "epoch": epoch,
+                    "training_participants": training_p_idx.keys(),
+                    "validation_participants": validation_p_idx.keys(),
+                    "test_participants": test_p_idx.keys()
+                }
+                save_pickle(save_data, "best_model.pkl", save_dir)
+
+        for key, val in metrics.items():
+            if isinstance(val, list):
+                if len(val):
+                    metrics[key] = np.mean(val)
+                else:
+                    metrics[key] = np.nan
+        logger.record_dict(metrics)
+        logger.dump_tabular(with_prefix=False, with_timestamp=False)
+    if save_model:
+        save_data = {"model": model, 
+                     "epoch": epoch,
+                     "training_participants": training_p_idx.keys(),
+                     "validation_participants": validation_p_idx.keys(),
+                     "test_participants": test_p_idx.keys()
+        }
         save_pickle(save_data, "model.pkl", save_dir)
 
 
