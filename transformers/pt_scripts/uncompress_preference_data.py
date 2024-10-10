01d54bb88ca0177375ab3effdbfe009a20ab1601
Loren Champlin <champlin@email.arizona.edu>
2024-10-10 16:14:34 -0700

    Fixed comments

--- transformers/pt_scripts/uncompress_preference_data.py
+++ transformers/pt_scripts/uncompress_preference_data.py
@@ -12,14 +12,14 @@ from transformers.data_utils.bb_data_loading import load_list
 
 def main(argv):
     parser = argparse.ArgumentParser(
-        description="Removes compression on preference data for multiple participants. \nThis does not check for compression, but will return \nuncompressed versions of datasets regardless. \nWarning: This edits the existing files, \nyou might want to make a backup of the data!",
+        description="Removes compression on preference data for multiple files. \nThis does not check for compression, but will return \nuncompressed versions of datasets regardless. \nWarning: This edits the existing files, \nyou might want to make a backup of the data!",
         formatter_class=StructuredFormatter,
     )
     parser.add_argument(
         "p_id",
         metavar="PID",
         type=str,
-        help="A .txt file containing a list of Participant IDs to process",
+        help="A .txt file containing a list of files (no extensions) to process",
     )
     parser.add_argument(
         "-d",
