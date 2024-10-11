e7c891c43dfb21fe4969529683a40f9bcef1edab
Loren Champlin <champlin@email.arizona.edu>
2024-10-11 11:46:08 -0700

    Updated

--- transformers/pt_scripts/run_pt_multiple.sh
+++ transformers/pt_scripts/run_pt_multiple.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-filename="$1"
+filename=$1
 
 while read -r line; do
-  echo "$line"
-done < "$filename"
+  CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}.hdf5" -o "${4}/${line}" -n 100 -b 256 -d 256 -w 8 -l 0.00001 0.0001 0.00001
+done < $filename
