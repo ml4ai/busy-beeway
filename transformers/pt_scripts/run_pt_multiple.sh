bf667e242af239c00743ca278078210254ee1b79
Loren Champlin <champlin@email.arizona.edu>
2024-10-12 10:04:06 -0700

    Fixed settings

--- transformers/pt_scripts/run_pt_multiple.sh
+++ transformers/pt_scripts/run_pt_multiple.sh
@@ -3,5 +3,5 @@
 filename=$1
 
 while read -r line; do
-  CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}.hdf5" -o "${4}/${line}" -n 10 -b 8 -d 256 -w 8 -l 0.0001 0.001 0.0001
+  CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}.hdf5" -o "${4}/${line}" -n 30 -b 8 -d 256 -w 4 -l 0.001 0.01 0.001
 done < $filename
