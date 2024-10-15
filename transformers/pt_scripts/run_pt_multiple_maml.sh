4018ca21fe706a99f18826e4ce68a7d351083bb1
Loren Champlin <champlin@email.arizona.edu>
2024-10-14 23:06:02 -0700

    For training models on maml

---
+++ transformers/pt_scripts/run_pt_multiple_maml.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filename=$1
+
+while read -r line; do
+  CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}.hdf5" -o "${4}/${line}" -n 10 -b 4 -d 256 -w 4 -l 0.00001 0.0001 0.00001 -p "${5}"
+done < $filename
