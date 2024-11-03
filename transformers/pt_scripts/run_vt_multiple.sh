bf293f0d2113672abfdab058883016a70dae73d4
Loren Champlin <champlin@email.arizona.edu>
2024-11-02 18:36:46 -0700

    script for running vt over multiple reward functions

---
+++ transformers/pt_scripts/run_vt_multiple.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filename=$1
+
+while read -r line; do
+  CUDA_VISIBLE_DEVICES=$2 python run_vt_training.py "${3}/${line}.hdf5" -o "${4}/${line}" -n 1000 -b 4 -d 4 -w 8 -l 0.00001 0.001 0.00001
+done < $filename
