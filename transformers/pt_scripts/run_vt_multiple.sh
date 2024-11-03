30479455255067a63f7cb8986b914a2c9b8af98e
Loren Champlin <champlin@email.arizona.edu>
2024-11-03 00:41:28 -0700

    Script for running multiple vt

--- transformers/pt_scripts/run_vt_multiple.sh
+++ transformers/pt_scripts/run_vt_multiple.sh
@@ -3,5 +3,5 @@
 filename=$1
 
 while read -r line; do
-  CUDA_VISIBLE_DEVICES=$2 python run_vt_training.py "${3}/${line}.hdf5" -o "${4}/${line}" -n 1000 -b 4 -d 4 -w 8 -l 0.00001 0.001 0.00001
+  CUDA_VISIBLE_DEVICES=$2 python run_vt_training.py "${3}/${line}.hdf5" -o "${4}/${line}" -n 1000 -b 4 -d 8 -w 8 -l 0.00001 0.001 0.00001
 done < $filename
