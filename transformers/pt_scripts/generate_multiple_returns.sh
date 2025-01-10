308df718d08c3ff59357e419a8d1d29365be755f
Loren Champlin <champlin@email.arizona.edu>
2025-01-10 14:52:22 -0700

    Testing

--- transformers/pt_scripts/generate_multiple_returns.sh
+++ transformers/pt_scripts/generate_multiple_returns.sh
@@ -2,7 +2,7 @@
 
 filename=$1
 
-count=0
+let count=0
 while read -r line; do
   #CUDA_VISIBLE_DEVICES=$2 python generate_return_samples.py "${3}/${line}/best_model.pkl" "${4}" -o "${5}" -t "${line}" &
   ((count++)) 
