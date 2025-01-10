0b13d84b8e35ba18555a2e50cfe6593070c56b4b
Loren Champlin <champlin@email.arizona.edu>
2025-01-10 09:39:27 -0700

    Can run multiple jobs

--- transformers/pt_scripts/generate_multiple_returns.sh
+++ transformers/pt_scripts/generate_multiple_returns.sh
@@ -2,6 +2,12 @@
 
 filename=$1
 
+count=0
 while read -r line; do
-  CUDA_VISIBLE_DEVICES=$2 python generate_return_samples.py "${3}/${line}/best_model.pkl" "${4}" -o "${5}" -t "${line}"
+  CUDA_VISIBLE_DEVICES=$2 python generate_return_samples.py "${3}/${line}/best_model.pkl" "${4}" -o "${5}" -t "${line}" &
+  (( count ++ )) 
+  if (( count = 10 )); then
+      wait
+      count=0
+  fi
 done < $filename
