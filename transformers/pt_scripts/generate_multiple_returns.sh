5d65be4aa0fe9386d7c7aeb91594822b334e79c2
Loren Champlin <champlin@email.arizona.edu>
2025-01-10 14:47:01 -0700

    Testing

--- transformers/pt_scripts/generate_multiple_returns.sh
+++ transformers/pt_scripts/generate_multiple_returns.sh
@@ -6,7 +6,7 @@ count=0
 while read -r line; do
   #CUDA_VISIBLE_DEVICES=$2 python generate_return_samples.py "${3}/${line}/best_model.pkl" "${4}" -o "${5}" -t "${line}" &
   (( count ++ )) 
-  if [ count == 10 ]; then
+  if (( count == 10 )); then
       echo "test"
       #wait
       count=0
