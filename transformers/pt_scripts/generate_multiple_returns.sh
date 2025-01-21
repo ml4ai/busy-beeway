142a42737d1ba512972357ae215d4585495c8781
Loren Champlin <champlin@email.arizona.edu>
2025-01-21 15:23:07 -0700

    For generating multiple returns for one set of data

--- transformers/pt_scripts/generate_multiple_returns.sh
+++ transformers/pt_scripts/generate_multiple_returns.sh
@@ -2,12 +2,6 @@
 
 filename=$1
 
-count=0
 while read -r line; do
-  CUDA_VISIBLE_DEVICES=$2 python generate_return_samples.py "${3}/${line}/best_model.pkl" "${4}" -o "${5}" -t "${line}" &
-  (( count ++ )) 
-  if (( count == 10 )); then
-      wait
-      count=0
-  fi
+  nice python generate_return_samples.py "${2}/${line}/best_model.pkl" "${3}" -p -f 50 -j -d "${4}" -e "${5}" -o "${6}" 
 done < $filename
