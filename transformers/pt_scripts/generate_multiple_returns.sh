4e52430f8d17aba9018fb827a6860ed00fe8b6e5
Loren Champlin <champlin@email.arizona.edu>
2025-01-23 15:19:57 -0700

    Not needed

--- transformers/pt_scripts/generate_multiple_returns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename=$1
-
-while read -r line; do
-  nice python generate_return_samples.py "${2}/${line}/best_model.pkl" "${3}" -p -f 50 -j -d "${4}" -e "${5}" -o "${6}" 
-done < $filename
