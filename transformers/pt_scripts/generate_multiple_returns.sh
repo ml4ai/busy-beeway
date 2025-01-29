741d865812523153887c78dcca20507a796cca33
Loren Champlin <champlin@email.arizona.edu>
2025-01-29 14:19:24 -0700

    Script for generating multiple returns

---
+++ transformers/pt_scripts/generate_multiple_returns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+filename=$1
+
+count=0
+while read -r line; do
+  CUDA_VISIBLE_DEVICES=$2 python generate_return_samples.py "${3}/${line}/best_model.pkl" "${4}" -o "${5}" -t "${line}" &
+  (( count ++ )) 
+  if (( count == 20 )); then
+      wait
+      count=0
+  fi
+done < $filename
