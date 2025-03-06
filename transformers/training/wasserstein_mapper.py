52574bab8ab91925c12453f7868c0449a0149638
Loren Champlin <champlin@email.arizona.edu>
2025-03-06 10:57:42 -0700

    Finished

--- transformers/training/wasserstein_mapper.py
+++ transformers/training/wasserstein_mapper.py
@@ -1,7 +1,19 @@
+import os
+from functools import partial
+
 import jax
 import jax.numpy as jnp
 import optax
+import orbax.checkpoint as ocp
 from flax import nnx
+from transformers.training.jax_utils import (
+    calculate_w_loss,
+    wasserstein_inner_loss_fn_gp,
+    wasserstein_inner_loss_fn_lp,
+    wasserstein_inner_loss_fn_nc,
+)
+from transformers.training.utils import ensure_dir
+
 
 class LipschitzFunction(nnx.Module):
     def __init__(self, dim, rngs: nnx.Rngs):
@@ -44,101 +56,109 @@ class WassersteinDistance:
         gp,
         lipschitz_f_dim,
         output_dim,
-        use_lipschitz_constraint=True,
         lipschitz_constraint_type="gp",
         wasserstein_lr=0.01,
+        rngs=nnx.Rngs(0, params=1, dropout=2),
     ):
         self.bnn = bnn
         self.gp = gp
         self.output_dim = output_dim
-        self.lipschitz_f_dim = lipschitz_f_dim
         self.lipschitz_constraint_type = lipschitz_constraint_type
-        assert self.lipschitz_constraint_type in ["gp", "lp"]
+        assert self.lipschitz_constraint_type in ["gp", "lp", None]
 
-        self.lipschitz_f = LipschitzFunction(dim=lipschitz_f_dim)
-        self.values_log = []
+        self.lipschitz_f = LipschitzFunction(dim=lipschitz_f_dim, rngs=rngs)
 
-        self.optimiser = nnx.optimizer.Optimizer(
-            self.lipschitz_f, optax.adagrad(wasserstein_lr)
-        )
-        self.use_lipschitz_constraint = use_lipschitz_constraint
-        self.penalty_coeff = 10
+        if lipschitz_constraint_type == "gp":
+            self.train_step = nnx.cached_partial(
+                _train_w_step_gp,
+                self.lipschitz_f,
+                nnx.Optimizer(self.lipschitz_f, optax.adagrad(wasserstein_lr)),
+                output_dim,
+                10,
+                rngs,
+            )
 
-    def compute_gradient_penalty(self, samples_p, samples_q, rngs: nnx.Rngs):
-        rng = rngs()
-        eps = jax.random.uniform(rng, (samples_p.shape[1], 1))
-        X = eps * samples_p.T + (1 - eps) * samples_q.T
-
-        if self.lipschitz_constraint_type == "gp":
-            # Gulrajani2017, Improved Training of Wasserstein GANs
-            return ((jnp.linalg.norm(vgrad(self.lipschitz_f, X), 2, 1) - 1) ** 2).mean()
-
-        elif self.lipschitz_constraint_type == "lp":
-            # Henning2018, On the Regularization of Wasserstein GANs
-            # Eq (8) in Section 5
-            return (
-                (
-                    jnp.clip(
-                        jnp.linalg.norm(vgrad(self.lipschitz_f, X), 2, 1) - 1,
-                        0.0,
-                        jnp.inf,
-                    )
-                )
-                ** 2
-            ).mean()
+        elif lipschitz_constraint_type == "lp":
+            self.train_step = nnx.cached_partial(
+                _train_w_step_lp,
+                self.lipschitz_f,
+                nnx.Optimizer(self.lipschitz_f, optax.adagrad(wasserstein_lr)),
+                output_dim,
+                10,
+                rngs,
+            )
+        else:
+            self.train_step = nnx.cached_partial(
+                _train_w_step_nc,
+                self.lipschitz_f,
+                nnx.Optimizer(self.lipschitz_f, optax.adagrad(wasserstein_lr)),
+            )
 
     def wasserstein_optimisation(
-        self, X, n_samples, n_steps=10, threshold=None, debug=False
+        self,
+        X,
+        n_samples,
+        n_steps=10,
+        threshold=None,
+        rngs=nnx.Rngs(0, params=1, dropout=2),
     ):
         n_samples_bag = n_samples * 1
 
         # Draw functions from GP
         gp_samples_bag = jnp.float32(
-            self.gp.sample_functions(jnp.float64(X), n_samples_bag)
+            self.gp.sample_functions(jnp.float64(X), n_samples_bag, rngs)
         )
-
         if self.output_dim > 1:
             gp_samples_bag = gp_samples_bag.squeeze()
 
         # Draw functions from Bayesian Neural network
-        nnet_samples_bag = jnp.float32(self.bnn.sample_functions(X, n_samples_bag))
+        nnet_samples_bag = jnp.float32(
+            self.bnn.sample_functions(X, n_samples_bag, rngs)
+        )
         if self.output_dim > 1:
-            nnet_samples_bag = nnet_samples_bag.squeeze())
+            nnet_samples_bag = nnet_samples_bag.squeeze()
+
+        batch = {"nnet_samples": nnet_samples_bag, "gp_samples": gp_samples_bag}
 
         for i in range(n_steps):
-            objective = -self.calculate(nnet_samples, gp_samples)
-            if debug:
-                self.values_log.append(-objective.item())
-
-            if self.use_lipschitz_constraint:
-                penalty = 0.0
-                for dim in range(self.output_dim):
-                    penalty += self.compute_gradient_penalty(
-                        nnet_samples[:, :, dim], gp_samples[:, :, dim]
-                    )
-                objective += self.penalty_coeff * penalty
-            objective.backward()
-
+            loss, grads = self.train_step(batch)
             if threshold is not None:
                 # Gradient Norm
-                params = self.lipschitz_f.parameters()
-                grad_norm = torch.cat([p.grad.data.flatten() for p in params]).norm()
+                grad_norm = jnp.linalg.norm(jax.flatten_util.ravel_pytree(grads)[0], 2)
 
-            self.optimiser.step()
-            if not self.use_lipschitz_constraint:
-                for p in self.lipschitz_f.parameters():
-                    p.data = torch.clamp(p, -0.1, 0.1)
+            if self.lipschitz_constraint_type is None:
+                p_state = nnx.state(self.lipschitz_f, nnx.Param)
+                new_p_state = jax.tree.map(lambda x: jnp.clip(x, -0.1, 0.1), p_state)
+                nnx.update(self.lipschitz_f, new_p_state)
             if threshold is not None and grad_norm < threshold:
                 print("WARNING: Grad norm (%.3f) lower than threshold (%.3f). ", end="")
                 print("Stopping optimization at step %d" % (i))
-                if debug:
-                    ## '-1' because the last wssr value is not recorded
-                    self.values_log = self.values_log + [self.values_log[-1]] * (
-                        n_steps - i - 1
-                    )
                 break
-        for p in self.lipschitz_f.parameters():
-            p.requires_grad = False
+
+
+@partial(nnx.jit, static_argnums=2)
+def _train_w_step_gp(model, optimizer, output_dim, penalty_coeff, rngs, batch):
+    loss, grads = nnx.value_and_grad(wasserstein_inner_loss_fn_gp)(
+        model, batch, output_dim, penalty_coeff, rngs
+    )
+    optimizer.update(grads)
+    return loss, grads
+
+
+@partial(nnx.jit, static_argnums=2)
+def _train_w_step_lp(model, optimizer, output_dim, penalty_coeff, rngs, batch):
+    loss, grads = nnx.value_and_grad(wasserstein_inner_loss_fn_lp)(
+        model, batch, output_dim, penalty_coeff, rngs
+    )
+    optimizer.update(grads)
+    return loss, grads
+
+
+@nnx.jit
+def _train_w_step_nc(model, optimizer, batch):
+    loss, grads = nnx.value_and_grad(wasserstein_inner_loss_fn_nc)(model, batch)
+    optimizer.update(grads)
+    return loss, grads
 
 
 class MapperWasserstein(object):
@@ -156,6 +176,7 @@ class MapperWasserstein(object):
         wasserstein_thres=0.01,
         logger=None,
         lipschitz_constraint_type="gp",
+        rngs=nnx.Rngs(0, params=1, dropout=2),
     ):
         self.gp = gp
         self.bnn = bnn
@@ -165,7 +186,7 @@ class MapperWasserstein(object):
         self.output_dim = output_dim
         self.out_dir = out_dir
 
-        assert lipschitz_constraint_type in ["gp", "lp"]
+        assert lipschitz_constraint_type in ["gp", "lp", None]
         self.lipschitz_constraint_type = lipschitz_constraint_type
 
         if type(wasserstein_steps) != list and type(wasserstein_steps) != tuple:
@@ -180,9 +201,8 @@ class MapperWasserstein(object):
             self.n_data,
             output_dim=self.output_dim,
             wasserstein_lr=wasserstein_lr,
-            device=self.device,
-            gpu_gp=self.gpu_gp,
             lipschitz_constraint_type=self.lipschitz_constraint_type,
+            rngs=rngs,
         )
 
         # Setup logger
@@ -195,63 +215,54 @@ class MapperWasserstein(object):
     def optimize(
         self,
         num_iters,
-        rngs: nnx.Rngs,
         n_samples=128,
         lr=1e-2,
         save_ckpt_every=50,
         print_every=10,
-        debug=False,
+        rngs=nnx.Rngs(0, params=1, dropout=2),
     ):
         wdist_hist = []
 
         wasserstein_steps = self.wasserstein_steps
-        prior_optimizer = torch.optim.RMSprop(self.bnn.parameters(), lr=lr)
-
+        checkpointer = ocp.StandardCheckpointer()
+        if self.output_dim > 1:
+            train_step = nnx.cached_partial(
+                _train_p_step_many,
+                self.bnn,
+                self.wasserstein.lipschitz_f,
+                nnx.Optimizer(self.bnn, optax.rmsprop(lr)),
+                rngs,
+                n_samples,
+            )
+        else:
+            train_step = nnx.cached_partial(
+                _train_p_step_one,
+                self.bnn,
+                self.wasserstein.lipschitz_f,
+                nnx.Optimizer(self.bnn, optax.rmsprop(lr)),
+                rngs,
+                n_samples,
+            )
         # Prior loop
         for it in range(1, num_iters + 1):
             # Draw X
             X = self.data_generator.get(self.n_data)
-            X = X.to(self.device)
-            if not self.gpu_gp:
-                X = X.to("cpu")
 
-            # Draw functions from GP
-            gp_samples = (
-                self.gp.sample_functions(X.double(), n_samples)
-                .detach()
-                .float()
-                .to(self.device)
+            gp_samples = jnp.float32(
+                self.gp.sample_functions(jnp.float64(X), n_samples, rngs)
             )
             if self.output_dim > 1:
                 gp_samples = gp_samples.squeeze()
-
-            if not self.gpu_gp:
-                X = X.to(self.device)
-
-            # Draw functions from BNN
-            nnet_samples = (
-                self.bnn.sample_functions(X, n_samples).float().to(self.device)
-            )
-            if self.output_dim > 1:
-                nnet_samples = nnet_samples.squeeze()
-
-            ## Initialisation of lipschitz_f
-            self.wasserstein.lipschitz_f.apply(weights_init)
-
             # Optimisation of lipschitz_f
             self.wasserstein.wasserstein_optimisation(
                 X,
                 n_samples,
                 n_steps=wasserstein_steps[1],
                 threshold=self.wasserstein_threshold,
-                debug=debug,
+                rngs=rngs,
             )
-            prior_optimizer.zero_grad()
-
-            wdist = self.wasserstein.calculate(nnet_samples, gp_samples)
-            wdist.backward()
-            prior_optimizer.step()
-
+            batch = {"gp_samples": gp_samples, "X": X}
+            wdist, grads = train_step(batch)
             wdist_hist.append(float(wdist))
             if (it % print_every == 0) or it == 1:
                 self.print_info(
@@ -262,13 +273,39 @@ class MapperWasserstein(object):
             # Save checkpoint
             if ((it) % save_ckpt_every == 0) or (it == num_iters):
                 path = os.path.join(self.ckpt_dir, "it-{}.ckpt".format(it))
-                torch.save(self.bnn.state_dict(), path)
-
-        # Save accumulated list of intermediate wasserstein values
-        if debug:
-            values = np.array(self.wasserstein.values_log).reshape(-1, 1)
-            path = os.path.join(self.out_dir, "wsr_intermediate_values.log")
-            np.savetxt(path, values, fmt="%.6e")
-            self.print_info("Saved intermediate wasserstein values in: " + path)
+                _, state = nnx.split(self.bnn)
+                checkpointer.save(path, state)
 
         return wdist_hist
+
+
+def calculate_w_loss_for_grad_one(bnn, lipschitz_f, rngs, n_samples, batch):
+    nnet_samples = jnp.float32(bnn.sample_functions(batch["X"], n_samples, rngs))
+
+    return calculate_w_loss(lipschitz_f, nnet_samples, batch["gp_samples"]).sum()
+
+
+def calculate_w_loss_for_grad_many(bnn, lipschitz_f, rngs, n_samples, batch):
+    nnet_samples = jnp.float32(bnn.sample_functions(batch["X"], n_samples, rngs))
+
+    return calculate_w_loss(
+        lipschitz_f, nnet_samples.squeeze(), batch["gp_samples"].squeeze()
+    ).sum()
+
+
+@partial(nnx.jit, static_argnums=4)
+def _train_p_step_one(bnn, lipschitz_f, optimizer, rngs, n_samples, batch):
+    loss, grads = nnx.value_and_grad(calculate_w_loss_for_grad_one)(
+        bnn, lipschitz_f, rngs, n_samples, batch
+    )
+    optimizer.update(grads)
+    return loss, grads
+
+
+@partial(nnx.jit, static_argnums=4)
+def _train_p_step_many(bnn, lipschitz_f, optimizer, rngs, n_samples, batch):
+    loss, grads = nnx.value_and_grad(calculate_w_loss_for_grad_many)(
+        bnn, lipschitz_f, rngs, n_samples, batch
+    )
+    optimizer.update(grads)
+    return loss, grads
