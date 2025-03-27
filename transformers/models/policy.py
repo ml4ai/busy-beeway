310808c21f84ef841fb2f312d2818bdd7f4a083e
Loren Champlin <champlin@email.arizona.edu>
2025-03-26 20:38:23 -0700

    IQL models

---
+++ transformers/models/policy.py
@@ -0,0 +1,99 @@
+import functools
+from typing import Optional, Sequence, Tuple
+import jax
+import jax.numpy as jnp
+import numpy as np
+from flax import nnx
+from tensorflow_probability.substrates import jax as tfp
+
+tfd = tfp.distributions
+tfb = tfp.bijectors
+
+from transformers.models.value_net import Identity, default_init, MLP
+
+class NormalTanhPolicy(nnx.Module):
+    def __init__(
+        self,
+        observation_dim: int,
+        mlp_output_dim: int,
+        hidden_dims: Sequence[int],
+        action_dim: int,
+        state_dependent_std: bool = True,
+        dropout_rate: Optional[float] = None,
+        log_std_scale: float = 1.0,
+        log_std_min: float = -10.0,
+        log_std_max: float = 2,
+        tanh_squash_distribution: bool = True,
+        rngs: nnx.Rngs = nnx.Rngs(0, params=1, dropout=2),
+    ):
+        self.mlp = MLP(
+            observation_dim,
+            mlp_output_dim,
+            hidden_dims,
+            activation_final=True,
+            dropout_rate=dropout_rate,
+            rngs=rngs,
+        )
+        self.mean_linear = nnx.Linear(
+            mlp_output_dim,
+            action_dim,
+            kernel_init=default_init(),
+            rngs=rngs,
+        )
+
+        if state_dependent_std:
+            self.get_log_stds = nnx.Linear(
+                mlp_output_dim,
+                action_dim,
+                kernel_init=default_init(log_std_scale),
+                rngs=rngs,
+            )
+        else:
+            self.log_stds = nnx.Param(jnp.zeros(action_dim))
+            self.get_log_stds = lambda x: self.log_stds.value
+
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+
+        if tanh_squash_distribution:
+            self.trans_mean = Identity()
+            self.get_dist = lambda dist: tfd.TransformedDistribution(
+                distribution=dist, bijector=tfb.Tanh()
+            )
+        else:
+            self.trans_mean = nnx.tanh
+            self.get_dist = lambda dist: dist
+
+    def __call__(
+        self,
+        observations: jax.Array,
+        temperature: float = 1.0,
+        training: bool = False,
+    ) -> tfd.Distribution:
+        outputs = self.mlp(observations, training=training)
+
+        means = self.mean_linear(outputs)
+
+        log_stds = self.get_log_stds(outputs)
+
+        log_stds = jnp.clip(log_stds, self.log_std_min, self.log_std_max)
+
+        means = self.trans_mean(means)
+
+        base_dist = tfd.MultivariateNormalDiag(
+            loc=means, scale_diag=jnp.exp(log_stds) * temperature
+        )
+
+        return self.get_dist(base_dist)
+
+
+@nnx.jit
+def sample_actions(
+    actor: nnx.Module,
+    observations: np.ndarray,
+    temperature: float = 1.0,
+    rngs: nnx.Rngs = nnx.Rngs(sample=4),
+) -> jax.Array:
+    dist = actor_def(observations, temperature)
+    key = rngs.sample()
+    return dist.sample(seed=key)
---
+++ transformers/models/value_net.py
@@ -0,0 +1,151 @@
+from typing import Optional, Sequence, Tuple
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+from flax import nnx
+
+
+class Identity(nnx.Module):
+    def __init__(self):
+        pass
+
+    def __call__(self, X: jax.Array, *args, **kwargs):
+        return X
+
+
+def default_init(scale: Optional[float] = jnp.sqrt(2)):
+    return nnx.initializers.orthogonal(scale)
+
+
+def init_dropout_layer(dropout_rate=None, rngs=nnx.Rngs(0, params=1, dropout=2)):
+    if dropout_rate is not None:
+        return nnx.Dropout(dropout_rate, rngs=rngs)
+    else:
+        return Identity()
+
+
+class MLP(nnx.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        hidden_dims: Sequence[int],
+        activations: Callable[[jax.Array], jax.Array] = nnx.relu,
+        activation_final: bool = False,
+        dropout_rate: Optional[float] = None,
+        rngs: nnx.Rngs = nnx.Rngs(0, params=1, dropout=2),
+    ):
+        self.layers = [
+            nnx.Linear(
+                input_dim,
+                hidden_dims[0],
+                kernel_init=default_init(),
+                rngs=rngs,
+            )
+        ]
+        self.dropout_layers = [init_dropout_layer(self.dropout_rate, rngs=rngs)]
+        for i in range(1, self.hidden_dims):
+            self.layers.append(
+                nnx.Linear(
+                    hidden_dims[i - 1],
+                    hidden_dims[i],
+                    kernel_init=default_init(),
+                    rngs=rngs,
+                )
+            )
+            self.dropout_layers.append(init_dropout_layer(self.dropout_rate, rngs=rngs))
+
+        self.output_layer = nnx.Linear(
+            hidden_dims[-1],
+            output_dim,
+            kernel_init=default_init(),
+            rngs=rngs,
+        )
+        self.activations = activations
+        if activation_final:
+            self.final_activation = activations
+            self.final_dropout = init_dropout_layer(self.dropout_rate, rngs=rngs)
+        else:
+            self.final_activation = Identity()
+            self.final_dropout = Identity()
+
+    def __call__(self, x: jax.Array, training: bool = False) -> jax.Array:
+        for linear_layer, drop_layer in zip(
+            list(self.layers), list(self.dropout_layers)
+        ):
+            x = drop_layer(
+                self.activations(linear_layer(x)), deterministic=not training
+            )
+
+        x = self.final_dropout(
+            self.final_activation(self.output_layer(x)), deterministic=not training
+        )
+
+        return x
+
+
+class ValueCritic(nnx.Module):
+    def __init__(
+        self,
+        observation_dim: int,
+        hidden_dims: Sequence[int],
+        rngs: nnx.Rngs = nnx.Rngs(0, params=1, dropout=2),
+    ):
+        self.mlp = MLP(observation_dim, hidden_dims, 1, rngs=rngs)
+
+    def __call__(self, observations: jax.Array) -> jax.Array:
+        critic = self.mlp(observations)
+        return jnp.squeeze(critic, -1)
+
+
+class Critic(nnx.Module):
+    def __init__(
+        self,
+        observation_dim: int,
+        action_dim: int,
+        hidden_dims: Sequence[int],
+        activations: Callable[[jax.Array], jax.Array] = nnx.relu,
+        rngs: nnx.Rngs = nnx.Rngs(0, params=1, dropout=2),
+    ):
+        self.mlp = MLP(
+            observation_dim + action_dim,
+            hidden_dims,
+            1,
+            activations=activations,
+            rngs=rngs,
+        )
+
+    def __call__(self, observations: jax.Array, actions: jax.Array) -> jax.Array:
+        inputs = jnp.concatenate([observations, actions], -1)
+        critic = self.mlp(inputs)
+        return jnp.squeeze(critic, -1)
+
+
+class DoubleCritic(nnx.Module):
+    def __init__(
+        self,
+        observation_dim: int,
+        action_dim: int,
+        hidden_dims: Sequence[int],
+        activations: Callable[[jax.Array], jax.Array] = nnx.relu,
+        rngs: nnx.Rngs = nnx.Rngs(0, params=1, dropout=2),
+    ):
+        self.critic1 = Critic(
+            observation_dim, action_dim, hidden_dims, activations=activations, rngs=rngs
+        )
+
+        self.critic2 = Critic(
+            observation_dim, action_dim, hidden_dims, activations=activations, rngs=rngs
+        )
+
+    def __call__(
+        self, observations: jax.Array, actions: jax.Array
+    ) -> Tuple[jax.Array, jax.Array]:
+        critic1 = self.critic1(
+            observations, actions
+        )
+        critic2 = self.critic2(
+            observations, actions
+        )
+        return critic1, critic2
