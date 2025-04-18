4c32544643e20ce2f26cf33b32543cf5f81765e0
Loren Champlin <champlin@email.arizona.edu>
2025-04-18 16:48:53 -0700

    fixed seeding

--- transformers/pt_scripts/run_multiple_pt.sh
+++ transformers/pt_scripts/run_multiple_pt.sh
@@ -2,6 +2,9 @@
 
 filename=$1
 
+counter=0
+seed=$5
 while read -r line; do
-  XLA_PYTHON_CLIENT_PREALLOCATE=false CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}_pref.hdf5" -o "${4}/${line}" -n 200 -b 256 -d 256 -w 20 -l 0.0 0.0001 0.0
+  XLA_PYTHON_CLIENT_PREALLOCATE=false CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}_pref.hdf5" -o "${4}/${line}" -n 200 -b 256 -d 256 -w 20 -l 0.0 0.0001 0.0 -s $((counter + seed))
+  counter=$((counter + 1))
 done < $filename
