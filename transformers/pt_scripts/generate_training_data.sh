81da9f201373a2c93c86deca997815d275feea4a
Loren Champlin <champlin@email.arizona.edu>
2025-04-18 00:22:48 -0700

    for generating pref training data

--- transformers/pt_scripts/generate_training_data.sh
+++ transformers/pt_scripts/generate_training_data.sh
@@ -2,6 +2,9 @@
 
 filename=$1
 
+counter=0
+seed=$5
 while read -r line; do
-  CUDA_VISIBLE_DEVICES=$2 python combine_state_data.py "${1}" -e "${line}" -d "${3}" -s "${3}/bbway1_no_${line}.hdf5"
+  CUDA_VISIBLE_DEVICES=$2 python create_pref_data.py "${1}" "${line}" -d "${3}" -s "${4}/${line}_pref.hdf5" -r $((counter + seed))
+  counter=$((counter + 1))
 done < $filename
