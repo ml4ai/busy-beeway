fe199df6a798e87596f17e4ed3534155119717e8
Loren Champlin <champlin@email.arizona.edu>
2025-04-18 19:02:59 -0700

    try two workers

--- transformers/pt_scripts/run_multiple_pt.sh
+++ transformers/pt_scripts/run_multiple_pt.sh
@@ -5,6 +5,6 @@ filename=$1
 counter=0
 seed=$5
 while read -r line; do
-  CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}_pref.hdf5" -o "${4}/${line}" -n 200 -b 256 -d 256 -w 0 -l 0.0 0.0001 0.0 -s $((counter + seed))
+  XLA_PYTHON_CLIENT_MEM_FRACTION=0.4 CUDA_VISIBLE_DEVICES=$2 python run_pt_training.py "${3}/${line}_pref.hdf5" -o "${4}/${line}" -n 200 -b 256 -d 256 -w 2 -l 0.0 0.0001 0.0 -s $((counter + seed))
   counter=$((counter + 1))
 done < $filename
