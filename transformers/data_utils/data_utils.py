59b5258ef8fbd92e96253d5e2526429ade7d5f4a
Loren Rieffer-Champlin <champlin@arizona.edu>
2026-04-20 23:20:01 -0700

    saved

--- transformers/data_utils/data_utils.py
+++ transformers/data_utils/data_utils.py
@@ -547,7 +547,7 @@ def create_state_data(
             ams.append(am)
 
             lbs.append(np.ones(s.shape[0]))
-        max(f_shapes)
+        print(max(f_shapes))
         if save_data is None:
             return {
                 labels[0]: np.concatenate(sts),
